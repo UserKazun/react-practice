b3212ae654cb690acf6a111c1d19f40441921e19
UserKazun <kazuusjlo8@gmail.com>
2022-04-16 21:55:16 +0900

    Fix to be able to use ts

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,13 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('app');
+    return view('welcome');
 });
 
+Auth::routes();
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-//Route::get('/login', [App\Http\Controllers\LoginController::class, 'index'])->name('login');
-
-Route::get('/login', function() {
-    return view('auth.login');
-})->name('login');
